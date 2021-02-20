7da4188d39fc7fee683b9b14753e1d7340a2ff2b
adhi <adhirajsingh7303@gmail.com>
2021-02-20 16:24:25 +0530

    Added assignment 6

---
+++ assignments-6.js
@@ -0,0 +1,18 @@
+// The sum of two numbers are : YOUR_ANSWER
+// The difference of two numbers are : YOUR_ANSWER
+// The product of two numbers are : YOUR_ANSWER
+// The division of two numbers are : YOUR_ANSWER
+// The remainder of two numbes are : YOUR_ANSWER
+// The square of number is: YOUR_ANSWER
+// The cube of the number is: YOUR_ANSWER
+
+// ====================================== //
+var a=20;
+var b=10;
+console.log('the sum of two number',eval(a+b))
+console.log('the diff of two number',eval(a-b))
+console.log('the product of two number',eval(a*b))
+console.log('the remainder of two number',eval(a%b))
+console.log('the division of two number',eval(a/b))
+console.log('the square of two number',eval(a**2))
+console.log('the sum of two number',eval(a**3))
