91acde3644036085da7561103fe2f454d81d1707
adhi <adhirajsingh7303@gmail.com>
2021-02-25 16:53:10 +0530

    assignment 10 done

---
+++ assignment-10.js
@@ -0,0 +1,21 @@
+// Instructions:- The Price for 1 ltr of petrol cost Rs. 97.
+// There are 4 Vehicles, given to you
+// A car has requires 10 Ltr of Petrol.
+// A bike requires 5 ltr of Petrol.
+// A Truck requires 30 ltr of Petrol.
+// A Bus requires 25 ltr of Petrol.
+
+// Calculate the cost of petrol required for each vehicle, and display the cost of each like
+// "The Cost of Petrol for a Car will be YOUR_ANSWER"
+
+// =========================== Your Code ============================== //
+var vehicle = "Car";
+var vehicleOne="bike";
+var vehicleTwo="Truck";
+var vehicleThree="Bus";
+ // bike, Truck, Bus
+var price= 97;
+console.log(`the cost of petrol for ${vehicle} will be ${10*price}`);
+console.log(`the cost of petrol for ${vehicleOne} will be ${5*price}`);
+console.log(`the cost of petrol for ${vehicleTwo} will be ${30*price}`);
+console.log(`the cost of petrol for ${vehicleThree} will be ${25*price}`);
