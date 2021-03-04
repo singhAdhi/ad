ca8a908769e07c8f0477b9f80b4fe86a4856eb42
ronin175 <pratikghagare1797@gmail.com>
2021-03-04 22:46:59 +0530

    Added Assignment 11 & 12

---
+++ assignment-11.js
@@ -0,0 +1,11 @@
+// Instructions:- Use Switch statements to perform the following code
+// There are various animals,
+
+// Zoo Animals - Lion, Tiger, Elephant
+// Pet Animals - Dog, Cat, Fish
+// Wild Animals - Bear, rihno
+
+// Display message for each of the category e.g This is a Zoo Animal or This is a wild Animal
+
+// ============================ Write your Code Below ========================================= //
+var animal;
